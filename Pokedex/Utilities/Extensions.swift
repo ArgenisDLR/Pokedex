8165ab2b1b83a13738eabb2de4d896b772610a35
Argenis De La Rosa <hennyrosexo@gmail.com>
2020-02-05 20:19:27 -0500

    created a second extension to ui color

--- Pokedex/Utilities/Extensions.swift
+++ Pokedex/Utilities/Extensions.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static func mainPink() -> UIColor {
+        return UIColor.rgb(red: 221, green: 94, blue: 86)
+    }
+}
+
 extension UIView {
     
     func center(inView view: UIView) {
