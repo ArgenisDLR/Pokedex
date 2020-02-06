eac6ca4ca2e81cb81f6ccb46a05b79026b0f409b
Argenis De La Rosa <hennyrosexo@gmail.com>
2020-02-05 21:29:33 -0500

    still need to update the scene delegate but removed the storyboard reference in the info.plist

--- Pokedex/Controller/PokedexController.swift
+++ Pokedex/Controller/PokedexController.swift
@@ -14,7 +14,9 @@ class PokedexController: UICollectionViewController {
     
     // MARK: - Properties
     
-    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     // MARK: - Init
 
@@ -25,14 +27,30 @@ class PokedexController: UICollectionViewController {
     
     }
     
+    // MARK: - Selectors
+    
+    @objc func showSearchBar() {
+        print(123)
+    }
+    
     // MARK: - Helper Functions
     
     func configureViewComponents() {
         
         collectionView.backgroundColor = .white
         
+        navigationController?.navigationBar.barTintColor = .mainPink()
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationItem.title = "Pokedex"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        navigationItem.rightBarButtonItem?.tintColor = .white
+        
         
     }
+    
+
 
 }
 
