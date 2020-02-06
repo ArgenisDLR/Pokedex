d85546959d2cff5d19ac6d77973838fe5248d703
Argenis De La Rosa <hennyrosexo@gmail.com>
2020-02-05 19:53:56 -0500

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by argenis delarosa on 2/5/20.
+//  Copyright © 2020 argenis delarosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
