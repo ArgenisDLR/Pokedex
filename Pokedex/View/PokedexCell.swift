f130d5e8007f73f6ee128ef57cd3e2efd7d501d0
Argenis De La Rosa <hennyrosexo@gmail.com>
2020-02-05 20:21:58 -0500

    created a container view to add an image view and label view

--- Pokedex/View/PokedexCell.swift
+++ Pokedex/View/PokedexCell.swift
@@ -12,6 +12,31 @@ class PokedexCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .groupTableViewBackground
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    lazy var nameContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .mainPink()
+        
+        view.addSubview(nameLabel)
+        nameLabel.center(inView: view)
+        
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Bulbasaur"
+        return label
+    }()
+    
     // MARK: - Init
     
     // MARK: - Helper Functions
